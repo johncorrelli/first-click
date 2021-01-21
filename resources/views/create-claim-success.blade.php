9f1601b9cf0af93662fdf66c6df8775802d1a7d0
John Correlli <john@teamgantt.com>
2021-01-20 23:20:22 -0500

    fix: resolve typo

--- resources/views/create-claim-success.blade.php
+++ resources/views/create-claim-success.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <h1>Almost there!</h1>
-<p>You just need to share your "Claim" and let people complete for the treasure!</p>
+<p>You just need to share your "Claim" and let people compete for the treasure!</p>
 
 <p class="alert alert-success"><a href='/claim/{{ $claimId }}'>Go to claim</a></p>
 <p><i>don't worry - it won't take the claim.</i></p>
